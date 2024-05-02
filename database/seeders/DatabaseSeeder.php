d9380c39cea859eadd6b316d7a2667cee6bd41aa
johannebert <johann.ebert@gmail.com>
2024-05-02 11:55:29 +0200

    * user seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
         ]);
+
+        User::factory(1000)->create();
     }
 }
