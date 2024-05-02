7d0e528383f03661737dc812f342ea1493e26331
johannebert <johann.ebert@gmail.com>
2024-05-02 13:28:00 +0200

    + one time notification

---
+++ app/Notifications/OneTimeNotification.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class OneTimeNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public string|array $data;
+
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database', 'mail'];
+    }
+
+    /**
+     * Determine which connections should be used for each notification channel.
+     *
+     * @return array<string, string>
+     */
+    public function viaConnections(): array
+    {
+        return [
+            'mail' => 'sync',
+            'database' => 'sync',
+        ];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the notification's database type.
+     *
+     * @return string
+     */
+    public function databaseType(object $notifiable): string
+    {
+        return 'one-time-notification';
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'data' => $this->data,
+            'subject' => $notifiable->name . ' has sent you a one-time notification.',
+        ];
+    }
+}
